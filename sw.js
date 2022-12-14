1a502861c7eaa170b7c8199c6937014b0b47cc45
tp-energy <120371610+tp-energy@users.noreply.github.com>
2022-12-14 14:24:36 +0800

    implemented service worker

---
+++ sw.js
@@ -0,0 +1,16 @@
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
+);
+
+workbox.routing.registerRoute(
+  ({ request }) => request.destination === "image",
+  new workbox.strategies.CacheFirst({
+    cacheName: "images",
+    plugins: [
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 50,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
